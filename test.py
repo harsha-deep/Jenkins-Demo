e9a7c139ecc952e43b5e7333530948a30e471eff
Harshadeep Donapati <77527524+harsha-deep@users.noreply.github.com>
2022-12-01 21:18:51 +0530

    Update test.py
    Added test cases

--- test.py
+++ test.py
@@ -4,14 +4,29 @@ from mult import multiply
 
 
 class Test(unittest.TestCase):
-    def test_addition(self):
+    def test_addition_1(self):
         lst = [1, 2]
         result = 3
         self.assertEqual(add(lst[0], lst[1]), result)
+    
+    def test_addition_2(self):
+        lst = [2, 3]
+        result = 5
+        self.assertEqual(add(lst[0], lst[1]), result)
 
-    def test_multiplication(self):
+    def test_multiplication_1(self):
         lst = [7, 7]
         result = 49
+        self.assertEqual(multiply(lst[0], lst[1]), result)
+    
+    def test_multiplication_2(self):
+        lst = [8, 8]
+        result = 72
+        self.assertEqual(multiply(lst[0], lst[1]), result)
+        
+    def test_multiplication_2(self):
+        lst = [10, 9]
+        result = 100
         self.assertEqual(multiply(lst[0], lst[1]), result)
 
 
